331f6efe2cccfdd74febe43db5284f287f09c28f
Aimar <aimarmndzz@gmail.com>
2024-10-18 18:59:43 +0100

    Prueba de comentado en script

---
+++ 01-Reto/bash.sh
@@ -0,0 +1,10 @@
+# Bash - Variables
+
+Crea tu primer programa en bash asegurándote de cumplir la sintaxi y dando permisos de ejecución al script creado.
+
+Crea un script con tres variables, una global, otra readonly y otra dentro de la función que sea local.
+Crea un función para realizar las pruebas.
+Saca el resultado de las variables con echo y demuestra en el script que entiendes cada tipo de variable.
+Comenta el script.
+Los nombres de las variables deben contener tu nombre para personalizar el script.
+
