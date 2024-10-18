9a98286bfe03efef990aab29ddec49332ca62a71
Aimar <aimarmndzz@gmail.com>
2024-10-18 19:01:54 +0100

    Segundo intento de comentado

--- 01-Reto/bash.sh
+++ 01-Reto/bash.sh
@@ -1,9 +1,9 @@
 # Bash - Variables
 
-Crea tu primer programa en bash asegurándote de cumplir la sintaxi y dando permisos de ejecución al script creado.
+# Crea tu primer programa en bash asegurándote de cumplir la sintaxi y dando permisos de ejecución al script creado.
 
-Crea un script con tres variables, una global, otra readonly y otra dentro de la función que sea local.
-Crea un función para realizar las pruebas.
+# "Crea un script con tres variables, una global, otra readonly y otra dentro de la función que sea local.
+Crea un función para realizar las pruebas."
 Saca el resultado de las variables con echo y demuestra en el script que entiendes cada tipo de variable.
 Comenta el script.
 Los nombres de las variables deben contener tu nombre para personalizar el script.
