fd7729c96afbfb9aef0d0b7d56eb96e61cece4f2
Aimar <aimarmndzz@gmail.com>
2024-10-20 02:30:59 +0100

    Reto 03

---
+++ 03-Reto/bash.sh
@@ -0,0 +1,7 @@
+# Bash - Arrays
+
+
+# Crea un array con los días de la semana.
+# Sácalos por pantalla con ayuda de un bucle.
+# Modifica los días de la semana para añadir el día Blinguis. Está ahí, entre el domingo y el lunes. El día tonto ese.
+
