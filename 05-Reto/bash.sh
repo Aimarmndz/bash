5510d24b9476dfd57a546f627618ea1549f04a3b
Aimar <aimarmndzz@gmail.com>
2024-10-20 02:34:08 +0100

    Reto 05

---
+++ 05-Reto/bash.sh
@@ -0,0 +1,5 @@
+# Bash - Conditional expression 
+
+# Lista los ficheros con permiso de ejecución de una carpeta. 
+# https://stackoverflow.com/questions/2437452/how-to-get-the-list-of-files-in-a-directory-in-a-shell-script
+
