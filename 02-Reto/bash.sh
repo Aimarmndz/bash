b0e2860cab6cac8237d367c3b5a10bf46d73d82f
Aimar <aimarmndzz@gmail.com>
2024-10-20 02:27:45 +0100

    Reto 02

---
+++ 02-Reto/bash.sh
@@ -0,0 +1,7 @@
+# Bash - Loop file
+# Bash - Comments
+
+# Lee un fichero de al menos 10 líneas con un bucle.
+# Escribe antes de cada línea del fichero el número de línea.
+# Crea un multiline comment dentro del script.
+
