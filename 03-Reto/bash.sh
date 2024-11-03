d4f853271499114f1c85fee891d6d048efdaf484
Aimar <aimarmndzz@gmail.com>
2024-11-03 17:51:11 +0000

    03-Resuelto y explicado

--- 03-Reto/bash.sh
+++ 03-Reto/bash.sh
@@ -5,3 +5,18 @@
 # Sácalos por pantalla con ayuda de un bucle.
 # Modifica los días de la semana para añadir el día Blinguis. Está ahí, entre el domingo y el lunes. El día tonto ese.
 
+#!/bin/bash
+
+dias_semana=("Lunes" "Martes" "Miércoles" "Jueves" "Viernes" "Sábado" "Domingo")
+
+echo "Días normales de la semana:"
+for dia in "${dias_semana[@]}"; do
+    echo "$dia"
+done
+
+dias_semana=("Blinguis" "${dias_semana[@]}")
+
+echo -e "\nDías de la semana con Blinguis:"
+for dia in "${dias_semana[@]}"; do
+    echo "$dia"
+done
