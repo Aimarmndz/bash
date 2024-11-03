40bf3143730712ba32c689270e61e99c1fdbbd22
Aimar <aimarmndzz@gmail.com>
2024-11-03 14:13:20 +0000

    02-Resuelto y explicado

--- 02-Reto/bash.sh
+++ 02-Reto/bash.sh
@@ -5,3 +5,18 @@
 # Escribe antes de cada línea del fichero el número de línea.
 # Crea un multiline comment dentro del script.
 
+#!/bin/bash
+
+archivo="notas.txt"
+
+if [[ ! -f "$archivo" ]]; then
+    echo "El archivo $archivo no existe."
+    exit 1
+fi
+
+contador=1
+
+while IFS= read -r linea; do
+    echo "$contador: $linea"
+    ((contador++))
+done < "$archivo"
