5ce22f6c038e047bbb0218bd7fab57f2c03c012d
Aimar <aimarmndzz@gmail.com>
2024-11-03 22:25:49 +0000

    05-Resuelto y explicado

--- 05-Reto/bash.sh
+++ 05-Reto/bash.sh
@@ -3,3 +3,15 @@
 # Lista los ficheros con permiso de ejecución de una carpeta. 
 # https://stackoverflow.com/questions/2437452/how-to-get-the-list-of-files-in-a-directory-in-a-shell-script
 
+#!/bin/bash
+
+directorio="/c/Users/aimar/Desktop"
+
+for archivo in "$directorio"/*; do
+    if [ -f "$archivo" ]; then
+        echo "$archivo"
+    fi
+done
+
+
+# No sé si el código está correcto o no, lee todos los archivos ejecutables
